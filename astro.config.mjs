aa4eaddd0c4b634c23f9a14870614dfa75bf5b15
Madeline Sparkles <madeline@mouse.lgbt>
2025-07-09 21:12:56 +0200

    fix deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,10 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
     site: "https://sqky.one",
+    prefetch: {
+        prefetchAll: true,
+        defaultStrategy: "viewport",
+    },
     experimental: {
         clientPrerender: true,
         contentIntellisense: true,
